ac8a0c733ceed3848d2f3e40b921ef3828434e1e
jin-Pro <dnjun2@ajou.ac.kr>
2022-06-01 15:14:03 +0900

    fix: SegmentTree Method Init Args

--- SegmentTree/SegmentTree.ts
+++ SegmentTree/SegmentTree.ts
@@ -1,45 +1,62 @@
-export class SegmentTree<T extends number>{
-  size:number;
-  max:number;
-  tree:T[] | number[];
-  node:T[] | number[];
+export class SegmentTree<T extends number> {
+  size: number;
+  max: number;
+  tree: T[] | number[];
+  node: T[] | number[];
 
-  constructor(arr:T[]){
+  constructor(arr: T[]) {
     this.max = arr.length;
     this.size = this.max * 4;
-    this.node = [0,...arr];
-    this.tree = [0,...arr];
-    this.init(0,1,this.max);
+    this.node = [0, ...arr];
+    this.tree = [0, ...arr];
+    this.init(0);
+    // this.init(0,1,this.max);
   }
 
-  init(index:number,start:number,end:number):T | number{
-    if(start === end){
-      this.tree[index] = this.node[start]
-      return this.tree[index]
+  init(index: number, start: number = 1, end: number = this.max): T | number {
+    if (start === end) {
+      this.tree[index] = this.node[start];
+      return this.tree[index];
     }
     let mid = Math.floor((start + end) / 2);
 
-    this.tree[index] = this.init(index * 2 + 1 , start, mid) + this.init(index * 2 + 2 , mid + 1, end);
+    this.tree[index] =
+      this.init(index * 2 + 1, start, mid) +
+      this.init(index * 2 + 2, mid + 1, end);
 
     return this.tree[index];
   }
 
-  update(index:number,target:number,diff:T,start:number,end:number){
-    if(target < start || target > end) return ;
+  update(
+    index: number,
+    target: number,
+    diff: T,
+    start: number = 1,
+    end: number = this.max - 1
+  ) {
+    if (target < start || target > end) return;
     this.tree[index] += diff;
-    if(start === end) this.node[start] += diff; 
+    if (start === end) this.node[start] += diff;
     else {
-      const mid = Math.floor((start + end ) / 2);
-      this.update(index * 2 + 1 , target , diff , start , mid);
-      this.update(index * 2 + 2 , target , diff , mid + 1 , end);
+      const mid = Math.floor((start + end) / 2);
+      this.update(index * 2 + 1, target, diff, start, mid);
+      this.update(index * 2 + 2, target, diff, mid + 1, end);
     }
   }
 
-  query(index:number,left:number,right:number,start:number,end:number):T | number{
-    if(left > end || right < start) return 0;
-    if(left <= start && end <= right ) return this.tree[index]
-    const mid = Math.floor((start + end ) / 2);
-    return this.query(index * 2 + 1 , left , right , start, mid) + 
-    this.query(index * 2 + 2 , left , right , mid + 1 , end);
+  query(
+    index: number,
+    left: number,
+    right: number,
+    start: number = 1,
+    end: number = this.max - 1
+  ): T | number {
+    if (left > end || right < start) return 0;
+    if (left <= start && end <= right) return this.tree[index];
+    const mid = Math.floor((start + end) / 2);
+    return (
+      this.query(index * 2 + 1, left, right, start, mid) +
+      this.query(index * 2 + 2, left, right, mid + 1, end)
+    );
   }
 }
--- SegmentTree/index.ts
+++ SegmentTree/index.ts
@@ -5,7 +5,7 @@ const arr = [1, 2, 3, 4, 5, 6, 7, 8];
 const tree = new SegmentTree(arr);
 console.log(tree.tree);
 
-console.log(tree.query(0, 3, 5, 1, arr.length - 1));
+console.log(tree.query(0, 3, 5));
 
-tree.update(0, 3, 10, 1, arr.length - 1);
+tree.update(0, 3, 10);
 console.log(tree.tree);
